e8d6a48ec62a012dd3c76b0213a5efd2515f9472
Malshan Rathnayake <malshan.edu@gmail.com>
2024-10-06 04:26:41 +0530

    account service interface

--- bank_data_web_business_layer/Interfaces/IAccountService.cs
+++ bank_data_web_business_layer/Interfaces/IAccountService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bank_data_web_models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace bank_data_web_business_layer.Interfaces
 {
     public interface IAccountService
     {
+        Task<bool> CreateNewBankAccount(Account account);
+        Task<Account> GetBankAccountByAccountNumber(string accountNumber);
+        Task<IEnumerable<Account>> GetBankAccountByAccountHolderName(string holderName);
+        Task<IEnumerable<Account>> GetAllBankAccounts();
+        Task<bool> UpdateBankAccount(Account account);
+        Task<bool> DeleteBankAccount(string accountNumber);
+
     }
 }
