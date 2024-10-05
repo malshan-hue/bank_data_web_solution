6a12ba97ebf9f777fb76848cd6f603cd93e639dc
Malshan Rathnayake <malshan.edu@gmail.com>
2024-10-06 04:47:04 +0530

    account table update

--- bank_data_web_database/Tables/Account.sql
+++ bank_data_web_database/Tables/Account.sql
@@ -9,6 +9,7 @@
 	[IsDeleted] BIT NOT NULL DEFAULT 0
 
 	CONSTRAINT [Account_AccountId_PK] PRIMARY KEY ([AccountId]),
-	CONSTRAINT [Account_UserId_FK] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId])
+	CONSTRAINT [Account_UserId_FK] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId]),
+	CONSTRAINT [Account_AccountNumber_Unique] UNIQUE ([AccountNumber])
 
 )
