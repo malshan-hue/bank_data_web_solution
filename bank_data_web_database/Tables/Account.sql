364fae1998a5d6807361a0f1f8e4d4fa908bdc78
Malshan Rathnayake <malshan.edu@gmail.com>
2024-10-06 02:32:13 +0530

    IsDeleted property for account

--- bank_data_web_database/Tables/Account.sql
+++ bank_data_web_database/Tables/Account.sql
@@ -5,7 +5,8 @@
 	[AccountNumber] NVARCHAR(200) NOT NULL,
 	[AccountHolderName] NVARCHAR(200) NOT NULL,
 	[Balance] DECIMAL(18,2) NOT NULL,
-	[CreatedDateTime] DATETIME NOT NULL
+	[CreatedDateTime] DATETIME NOT NULL,
+	[IsDeleted] BIT NOT NULL
 
 	CONSTRAINT [Account_AccountId_PK] PRIMARY KEY ([AccountId]),
 	CONSTRAINT [Account_UserId_FK] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId])
--- bank_data_web_models/Account.cs
+++ bank_data_web_models/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace bank_data_web_models
         public string AccountHolderName { get; set; } = string.Empty;
         public decimal Balance { get; set; }
         public DateTime CreatedDateTime { get; set; }
+        [DefaultValue("false")]
+        public bool IsDeleted { get; set; }
 
         #region NAVIGATIONAL PROPERTIES
         public UserInformation UserInformation { get; set; } = new UserInformation();
